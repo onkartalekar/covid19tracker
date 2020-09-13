46646b5dc6cde91e546ae56fc7bedc2de7f20d99
Onkar Talekar <onkar.talekar@gmail.com>
2020-09-13 00:13:52 -0400

    First cut - covid 19 tracker data for India

---
+++ client/js/controller.js
@@ -0,0 +1,294 @@
+angular.module("app", ["chart.js", 'datatables'])
+    // Optional configuration
+    .config(['ChartJsProvider', function (ChartJsProvider) {
+        // Configure all charts
+        ChartJsProvider.setOptions({
+            chartColors: ['#ff0000', '#008000', '#0000ff'],
+            responsive: true
+        });
+        // Configure all line charts
+        ChartJsProvider.setOptions('line', {
+            showLines: true
+        });
+    }])
+    .controller("CovidSummaryController", ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
+
+        $scope.tabs = [{
+            href: "states",
+            title: "States Level Data",
+            isActive: true
+        }, {
+            href: "districts",
+            title: "Districts Level Data",
+            isActive: false
+        }];
+
+        $scope.setActiveTab = function (index) {
+            $scope.tabs.forEach(tab => tab.isActive = false);
+            $scope.tabs[index].isActive = true;
+        }
+
+        $scope.shouldShowGraphForState = [];
+        let effectiveDate = new Date();
+        effectiveDate.setDate(new Date().getDate() - 1);
+        $scope.statesLookup = {
+            "AP": "Andhra Pradesh",
+            "AR": "Arunachal Pradesh",
+            "AS": "Assam",
+            "BR": "Bihar",
+            "CT": "Chhattisgarh",
+            "GA": "Goa",
+            "GJ": "Gujarat",
+            "HR": "Haryana",
+            "HP": "Himachal Pradesh",
+            "JK": "Jammu and Kashmir",
+            "JH": "Jharkhand",
+            "KA": "Karnataka",
+            "KL": "Kerala",
+            "MP": "Madhya Pradesh",
+            "MH": "Maharashtra",
+            "MN": "Manipur",
+            "ML": "Meghalaya",
+            "MZ": "Mizoram",
+            "NL": "Nagaland",
+            "OR": "Odisha",
+            "PB": "Punjab",
+            "RJ": "Rajasthan",
+            "SK": "Sikkim",
+            "TG": "Telangana",
+            "TN": "Tamil Nadu",
+            "TR": "Tripura",
+            "UT": "Uttarakhand",
+            "UP": "Uttar Pradesh",
+            "WB": "West Bengal",
+            "AN": "Andaman and Nicobar Islands",
+            "DN": "Dadra Nagar Haveli and Daman Diu",
+            "DL": "Delhi",
+            "PY": "Pondicherry",
+            "LA": "Ladakh",
+            "CH": "Chandigarh"
+        };
+
+        $scope.showHideGraphForState = function (state) {
+            $scope.shouldShowGraphForState[state] = !$scope.shouldShowGraphForState[state];
+        };
+
+        $scope.calculateRate = function (cases, confirmed) {
+            if (cases === undefined || cases == null || confirmed === 0) {
+                return '0%';
+            }
+            return Math.round(cases / confirmed * 10000) / 100 + '%';
+        }
+
+        let apiContext = "http://www.vishwaroop.info";
+        //let apiContext = "http://localhost:8080";
+        $http.get(apiContext + "/timeseries")
+            .then(function (data) {
+                $scope.seriesData = {};
+                $scope.labels = {};
+                $scope.series = ['confirmed', 'recovered'];
+                $scope.testedGraphSeries = ['tested'];
+                $scope.deceasedGraphSeries = ['deceased'];
+                $scope.statesFromTimeseries = Object.keys(data.data);
+                let totalConfirmed = [], totalRecovered = [], totalDeceased = [], totalTested = [], deltaConfirmed = [],
+                    deltaRecovered = [], deltaDeceased = [], deltaTested = [];
+
+                for (const key in $scope.statesFromTimeseries) {
+                    const state = $scope.statesFromTimeseries[key];
+                    $scope.labels[state] = Object.keys(data.data[state].dates);
+
+                    for (const date in data.data[state].dates) {
+                        if (data.data[state].dates.hasOwnProperty(date)) {
+                            const day = data.data[state].dates[date];
+
+                            if (day.total) {
+                                totalConfirmed.push(day.total.confirmed);
+                                totalRecovered.push(day.total.recovered);
+                                totalDeceased.push(day.total.deceased);
+                                totalTested.push(day.total.tested);
+                            }
+                            if (day.delta) {
+                                deltaConfirmed.push(day.delta.confirmed);
+                                deltaRecovered.push(day.delta.recovered);
+                                deltaDeceased.push(day.delta.deceased);
+                                deltaTested.push(day.delta.tested);
+                            }
+                        }
+                    }
+
+                    if (state === 'TT') {
+                        $scope.nationalSeriesData = [];
+                        $scope.nationalTestedSeriesData = [];
+                        $scope.nationalDeceasedSeriesData = [];
+                        $scope.nationalSeriesData.push(totalConfirmed);
+                        $scope.nationalSeriesData.push(totalRecovered);
+                        $scope.nationalDeceasedSeriesData.push(totalDeceased);
+                        $scope.nationalTestedSeriesData.push(totalTested);
+
+                        $scope.nationalStats = {};
+                        $scope.nationalStats.total = {
+                            'confirmed': totalConfirmed[totalConfirmed.length - 1],
+                            'deceased': totalDeceased[totalDeceased.length - 1],
+                            'recovered': totalRecovered[totalRecovered.length - 1],
+                            'tested': totalTested[totalTested.length - 1],
+                            'recoveryRate': $scope.calculateRate(totalRecovered[totalRecovered.length - 1], totalConfirmed[totalConfirmed.length - 1]),
+                            'mortalityRate': $scope.calculateRate(totalDeceased[totalDeceased.length - 1], totalConfirmed[totalConfirmed.length - 1])
+                        };
+                        $scope.nationalStats.delta = {
+                            'confirmed': deltaConfirmed[totalConfirmed.length - 1],
+                            'deceased': deltaDeceased[totalDeceased.length - 1],
+                            'recovered': deltaRecovered[totalRecovered.length - 1],
+                            'tested': deltaTested[deltaTested.length - 1]
+                        };
+
+                    } else {
+                        $scope.seriesData[state] = [];
+                        $scope.seriesData[state].push(totalConfirmed)
+                        $scope.seriesData[state].push(totalRecovered);
+                        $scope.seriesData[state].push(totalDeceased);
+                    }
+
+                    //reset
+                    totalConfirmed = [];
+                    totalRecovered = [];
+                    totalDeceased = [];
+                    totalTested = [];
+                    deltaConfirmed = [];
+                    deltaRecovered = [];
+                    deltaDeceased = [];
+                    deltaTested = [];
+                }
+
+            }, function (error) {
+                console.log(error);
+            });
+
+        let summaryDateParameter = effectiveDate.toISOString().substring(0, 10);
+        $http.get(apiContext + "/summary/" + summaryDateParameter)
+            .then(function (data) {
+                $scope.effectiveDate = effectiveDate.toDateString();
+                if (data.data === 'No data found') {
+                    console.log('No data found for ' + summaryDateParameter + '.');
+                    effectiveDate.setDate(effectiveDate.getDate() - 1);
+                    summaryDateParameter = effectiveDate.toISOString().substring(0, 10);
+
+                    $http.get(apiContext + "/summary/" + summaryDateParameter)
+                        .then(function (data) {
+                            if (data.data === 'No data found') {
+                                console.log('No data found for ' + summaryDateParameter + '. Please try again later.');
+                            } else {
+                                fillSummaryData(data.data);
+                            }
+                        }, function (error) {
+                            console.log(error);
+                        });
+                } else {
+                    fillSummaryData(data.data);
+                }
+            }, function (error) {
+                console.log(error);
+            });
+
+        function fillSummaryData(summary) {
+            $scope.summary = summary;
+            $scope.states = Object.keys(summary);
+
+            $scope.states.forEach(state => {
+                $scope.summary[state].population = summary[state].meta.population;
+                $scope.summary[state].total.testingRate = $scope.calculateRate(summary[state].total.tested, summary[state].population);
+                $scope.summary[state].total.confirmedRate = $scope.calculateRate(summary[state].total.confirmed, summary[state].total.tested);
+                $scope.summary[state].total.recoveryRate = $scope.calculateRate(summary[state].total.recovered, summary[state].total.confirmed);
+                $scope.summary[state].total.mortalityRate = $scope.calculateRate(summary[state].total.deceased, summary[state].total.confirmed);
+
+
+                if (state !== 'TT') {
+                    $scope.summary[state].districtNames = Object.keys(summary[state].districts);
+                    $scope.summary[state].districtNames.forEach(district => {
+                        if (summary[state].districts[district]) {
+                            if (summary[state].districts[district].meta) {
+                                $scope.summary[state].districts[district].population = summary[state].districts[district].meta.population;
+                            }
+                            if (summary[state].districts[district].total) {
+                                $scope.summary[state].districts[district].total.recoveryRate = $scope.calculateRate(summary[state].districts[district].total.recovered, summary[state].districts[district].total.confirmed);
+                                $scope.summary[state].districts[district].total.mortalityRate = $scope.calculateRate(summary[state].districts[district].total.deceased, summary[state].districts[district].total.confirmed);
+                            }
+                        } else {
+                            $scope.summary[state].districts[district].total = {
+                                'confirmed': null,
+                                'deceased': null,
+                                'recovered': null,
+                                'recoveryRate': null,
+                                'mortalityRate': null
+                            };
+                        }
+                        if (!summary[state].districts[district].delta) {
+                            $scope.summary[state].districts[district].delta = {
+                                'confirmed': null,
+                                'deceased': null,
+                                'recovered': null
+                            };
+                        }
+                    });
+                }
+            });
+        }
+
+        $scope.formatNumberToString = function (number, isSignedNumber) {
+            if (number === undefined || number == null || number === 0) {
+                return '-';
+            }
+
+            let formattedString;
+            let sign = '';
+            if (isSignedNumber) {
+                sign = number < 0 ? '-' : '+';
+            }
+            number = Math.abs(number);
+            let quotient = Math.floor(number / 1000);
+            let reminder = number % 1000;
+
+            if (quotient > 0) {
+                formattedString = ',' + reminder.toString().padStart(3, '0');
+
+                reminder = quotient % 100;
+                quotient = Math.floor(quotient / 100);
+
+                if (quotient > 0) {
+                    formattedString = ',' + reminder.toString().padStart(2, '0') + formattedString;
+
+                    reminder = quotient % 100;
+                    quotient = Math.floor(quotient / 100);
+
+                    if (quotient > 0) {
+                        formattedString = ',' + reminder.toString().padStart(2, '0') + formattedString;
+
+                        reminder = quotient % 100;
+                        quotient = Math.floor(quotient / 100);
+
+                        if (quotient > 0) {
+                            formattedString = ',' + reminder.toString().padStart(2, '0') + formattedString;
+
+                            reminder = quotient % 100;
+                            quotient = Math.floor(quotient / 100);
+
+                            if (quotient > 0) {
+                                return 'large number';
+                            } else {
+                                return sign + reminder + formattedString;
+                            }
+                        } else {
+                            return sign + reminder + formattedString;
+                        }
+                    } else {
+                        return sign + reminder + formattedString;
+                    }
+                } else {
+                    return sign + reminder + formattedString;
+                }
+
+            } else {
+                return sign + reminder.toString();
+            }
+        }
+
+    }]);
---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express'),
+    app = new express(),
+    request = require('request');
+
+app.use(express.static('client'));
+app.use('/', express.static('client/html'))
+app.use('/js', express.static('client/js'));
+app.use('/css', express.static('client/css'));
+app.use('/webfonts', express.static('client/webfonts'))
+
+const port = 8080;
+app.listen(port, () => {
+    console.log("Server started on port " + port);
+})
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+app.get('/timeseries', function (req, res) {
+
+    request.get('https://api.covid19india.org/v4/timeseries.json', {json: true}, (error, response, body) => {
+        if (error) {
+            return console.log('error:' + error);
+        }
+
+        res.send(body);
+    });
+});
+
+app.get('/summary/:summaryDate', function (req, res) {
+    let summaryDate = req.params.summaryDate;
+    let uri = 'https://api.covid19india.org/v4/data-' + summaryDate + '.json';
+    request.get(uri, {json: true}, (error, response, body) => {
+        let responseJSON = JSON.parse(JSON.stringify(response));
+        if (responseJSON.statusCode == 404) {
+            res.send("No data found");
+        } else {
+            res.send(body);
+        }
+    });
+});
